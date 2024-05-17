e3cb1ddd99d9db20e5c937ac3c5f0c683b762f9e
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2024-05-17 22:32:07 +0600

    create an alert(),prompt(),confirm()

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var name = "md moniruzzaman arafat";
+/* var name = "md moniruzzaman arafat";
 let message = "How are you";
 const PI = 3.1416;
-console.log(name, message, PI);
+console.log(name, message, PI); */
 
 // data type in javaScript
-let number = 10;
+/* let number = 10;
 let string = "hello";
 let boolean = true;
 let nullValue = null;
@@ -15,4 +15,9 @@ console.log(typeof string);
 console.log(typeof boolean);
 console.log(typeof nullValue);
 console.log(typeof noDefineValue);
-console.log(typeof alert);
+console.log(typeof alert); */
+
+// alert, prompt, confirm
+alert("hello world");
+prompt();
+confirm();
