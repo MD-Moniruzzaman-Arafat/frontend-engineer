7b9c757ed68cd6d89d8187d6b391ac573337e349
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2024-05-17 22:15:47 +0600

    find the datatype for using typeof function

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ var name = "md moniruzzaman arafat";
 let message = "How are you";
 const PI = 3.1416;
 console.log(name, message, PI);
+
+// data type in javaScript
+let number = 10;
+let string = "hello";
+let boolean = true;
+let nullValue = null;
+let noDefineValue;
+let alert = function alert() {};
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof nullValue);
+console.log(typeof noDefineValue);
+console.log(typeof alert);
