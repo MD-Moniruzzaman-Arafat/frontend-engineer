df644c773233798ebf460876b3463ae9b3c5993e
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2024-05-18 10:12:55 +0600

    learn basic operator and string concat

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ prompt();
 confirm(); */
 
 // type conversion
-const number = 100;
+/* const number = 100;
 const stringConversion = String(number);
 console.log(stringConversion);
 
@@ -38,3 +38,22 @@ console.log(numberConversion2);
 const boolean1 = false;
 const stringConversion3 = String(boolean1);
 console.log(stringConversion3);
+ */
+
+// basic operator
+let a = 10;
+let b = 5;
+const add = a + b;
+const sum = a - b;
+const mul = a * b;
+const div = a / b;
+const reminder = a % b;
+const exponential = a ** b;
+
+console.log(add, sum, mul, div, reminder, exponential);
+
+// string concat
+let firstName = "Moniruzzaman ";
+let lastName = "Arafat";
+const fullName = firstName + lastName;
+console.log(fullName);
